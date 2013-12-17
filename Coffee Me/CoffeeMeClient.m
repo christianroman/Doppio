3587e61575efac1909c15a71f58458dbf1097f0f
Christian Roman <chroman16@gmail.com>
2013-12-17 14:13:41 -0600

    AFNetworking 1.3.3

---
+++ Coffee Me/CoffeeMeClient.m
@@ -0,0 +1,13 @@
+//
+//  CoffeeMeClient.m
+//  Coffee Me
+//
+//  Created by Christian Roman on 17/12/13.
+//  Copyright (c) 2013 Christian Roman. All rights reserved.
+//
+
+#import "CoffeeMeClient.h"
+
+@implementation CoffeeMeClient
+
+@end
---
+++ Coffee MeTests/Coffee_MeTests.m
@@ -0,0 +1,34 @@
+//
+//  Coffee_MeTests.m
+//  Coffee MeTests
+//
+//  Created by Christian Roman on 17/12/13.
+//  Copyright (c) 2013 Christian Roman. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Coffee_MeTests : XCTestCase
+
+@end
+
+@implementation Coffee_MeTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
